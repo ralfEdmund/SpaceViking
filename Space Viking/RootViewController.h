762455c1eeb83e53205e3fcfdf2f7d5a5cdcfbbf
Ralf E. Stranzenbach <ralf@reswi.de>
2011-10-06 23:24:15 +0200

    Initial Commit

---
+++ Space Viking/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Space Viking
+//
+//  Created by Stranzenbach Ralf on 06.10.11.
+//  Copyright BearingPoint GmbH 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Space Viking/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Space Viking
+//
+//  Created by Stranzenbach Ralf on 06.10.11.
+//  Copyright BearingPoint GmbH 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
